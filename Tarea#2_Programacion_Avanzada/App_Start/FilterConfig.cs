77165c7ed96fcc0010cf17f6b9c8c908e3586796
Joseph Gonzalez Herrera <goteks28@gmail.com>
2026-06-23 10:57:56 -0600

    Add project files.

---
+++ Tarea#2_Programacion_Avanzada/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea_2_Programacion_Avanzada
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Tarea#2_Programacion_Avanzada/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tarea_2_Programacion_Avanzada.MvcApplication" Language="C#" %>
